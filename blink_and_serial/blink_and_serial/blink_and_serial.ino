54b27e0abc9f91ee09f88324a605de8522762797
mariusz-pazur <mariusz.pazur@gmail.com>
2014-02-08 14:03:37 +0100

    First arduino sketch - blink and serial

---
+++ blink_and_serial/blink_and_serial/blink_and_serial.ino
@@ -0,0 +1,28 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+
+void setup() 
+{                
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);  
+  Serial.begin(19200);  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  Serial.write("Czesc\r\n");
+  digitalWrite(led1, HIGH); 
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  delay(100);               
+  digitalWrite(led1, LOW); 
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  delay(100);   
+  digitalWrite(led1, LOW); 
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  delay(100);
+}
