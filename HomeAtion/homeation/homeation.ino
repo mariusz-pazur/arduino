1acff6a050e7c408c65f9e8830316728b32c3663
mariusz-pazur <mariusz.pazur@gmail.com>
2014-05-14 00:33:59 +0200

    Schematy HomeAtionMain i HomeAtionClient
    testowanie NRF24 + moduł Ethernet

--- HomeAtion/homeation/homeation.ino
+++ HomeAtion/homeation/homeation.ino
@@ -1,11 +1,31 @@
+/*
+  Web Server
+ 
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield. 
+ 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+ 
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ 
+ */
+
 #include <SPI.h>
+#include <UIPEthernet.h>
 #include <RF24.h>
 #include "printf.h"
 
-//
-// Hardware configuration
-//
-// Set up nRF24L01 radio on SPI bus plus pins 7 & 8
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,0,6);
+
 RF24 radio(7,8);
 const uint64_t remoteAddress = 0xF0F0F0F0D2LL;
 const uint64_t myAddress = 0xF0F0F0F0E1LL;
@@ -14,9 +34,24 @@ const int command_pin = 9;
 int commands[] = {0, 1};
 int commandToSend = 0;
 
-void setup(void)
-{
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+ // Open serial communications and wait for port to open:
   Serial.begin(57600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
   printf_begin();
   printf("HomeAtion Main\n\r");
 
@@ -37,9 +72,9 @@ void setup_RF(void)
   radio.printDetails();
 }
 
-void loop(void)
+void sendRFCommand()
 {
-    // First, stop listening so we can talk.
+  // First, stop listening so we can talk.
     radio.stopListening();
 
     //choose command
@@ -90,6 +125,51 @@ void loop(void)
     }
 
     // Try again 1s later
-    delay(1000);
+    //delay(1000);
 }
-// vim:cin:ai:sts=2 sw=2 ft=cpp
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response	  
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");          
+          client.println("OK!");
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+    sendRFCommand();    
+  }
+}
+
