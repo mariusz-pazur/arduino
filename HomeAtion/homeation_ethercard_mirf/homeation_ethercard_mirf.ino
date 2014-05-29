0881d212a24f5f7662c941b9688417b22eee812a
mariusz-pazur <mariusz.pazur@gmail.com>
2014-05-29 23:36:54 +0200

    HA_RemotePower na MIRF

--- HomeAtion/homeation_ethercard_mirf/homeation_ethercard_mirf.ino
+++ HomeAtion/homeation_ethercard_mirf/homeation_ethercard_mirf.ino
@@ -9,19 +9,24 @@
 #define STATIC 1
 #define HOME_ATION_DEBUG 1
 
-static uint8_t remoteAddress[] =  {0xF0, 0xF0, 0xF0, 0xF0, 0xD2};
-static uint8_t myAddress[] = {0xF0, 0xF0, 0xF0, 0xF0, 0xE1 };
+static uint8_t remoteAddress[] =  {
+  0xF0, 0xF0, 0xF0, 0xF0, 0xD2};
+static uint8_t myAddress[] = {
+  0xF0, 0xF0, 0xF0, 0xF0, 0xE1 };
 byte commandToSend[4];
 
 #if STATIC
 // ethernet interface ip address
-static byte myip[] = { 192,168,0,6 };
+static byte myip[] = { 
+  192,168,0,6 };
 // gateway ip address
-static byte gwip[] = { 192,168,0,1 };
+static byte gwip[] = { 
+  192,168,0,1 };
 #endif
 
 // ethernet mac address - must be unique on your network
-static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+static byte mymac[] = { 
+  0x74,0x69,0x69,0x2D,0x30,0x31 };
 
 byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
 BufferFiller bfill;
@@ -44,48 +49,50 @@ void setup()
   setupRF();  
 #if HOME_ATION_DEBUG
   printf("HomeAtion Main\n\r");
-	printf("Server is at %d.%d.%d.%d\n\r", ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3]);  
+  printf("Server is at %d.%d.%d.%d\n\r", ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3]);  
 #endif
   /*lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print(ether.myip[0]);
-  lcd.print('.');
-  lcd.print(ether.myip[1]);
-  lcd.print('.');
-  lcd.print(ether.myip[2]);
-  lcd.print('.');
-  lcd.print(ether.myip[3]);*/
+   lcd.clear();
+   lcd.print(ether.myip[0]);
+   lcd.print('.');
+   lcd.print(ether.myip[1]);
+   lcd.print('.');
+   lcd.print(ether.myip[2]);
+   lcd.print('.');
+   lcd.print(ether.myip[3]);*/
+   Serial.println("\n[memCheck]");
+   Serial.println(freeRam());
 }
 
 void setupRF()
 {
-	Mirf.cePin = 7;
-	Mirf.csnPin = 8;
-	Mirf.spi = &MirfHardwareSpi;
-	Mirf.init();
-	Mirf.setRADDR(myAddress);
-	Mirf.payload = 4;
-	Mirf.channel = 76;
-	Mirf.config(); 
+  Mirf.cePin = 7;
+  Mirf.csnPin = 8;
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR(myAddress);
+  Mirf.payload = 4;
+  Mirf.channel = 76;
+  Mirf.config(); 
 }
 
 void setupEthernet()
 {
-	if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
-	{
+  if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0) 
+  {
 #if HOME_ATION_DEBUG
-		printf("Failed to access Ethernet controller\n\r");
+    printf("Failed to access Ethernet controller\n\r");
 #endif
-	}
+  }
 #if STATIC
-	ether.staticSetup(myip, gwip);
+  ether.staticSetup(myip, gwip);
 #else
-	if (!ether.dhcpSetup())
-	{
+  if (!ether.dhcpSetup())
+  {
 #if HOME_ATION_DEBUG
-		printf("DHCP failed\n\r");
+    printf("DHCP failed\n\r");
 #endif
-	}
+  }
 #endif
 }
 
@@ -98,102 +105,109 @@ void setupEthernet()
 //		  - 5 - disable all
 boolean sendRFCommand(byte* commandArray, uint8_t* response)
 {    
-    // First, stop listening so we can talk.    
+  // First, stop listening so we can talk.    
 #ifdef HOME_ATION_DEBUG
-    printf("Now sending (%d-%d-%d)", commandArray[0], commandArray[1], commandArray[2]);
+  printf("Now sending (%d-%d-%d)", commandArray[0], commandArray[1], commandArray[2]);
 #endif
-    Mirf.setTADDR(remoteAddress);
-	Mirf.send(commandArray);
-    while(Mirf.isSending())
-	{
-	}
-    delay(10);
-	unsigned long started_waiting_at = millis();
-	while(!Mirf.dataReady())
-	{    
-		if ( ( millis() - started_waiting_at ) > 1000 ) 
-		{
+  Mirf.setTADDR(remoteAddress);
+  Mirf.send(commandArray);
+  while(Mirf.isSending())
+  {
+  }
+  delay(10);
+  unsigned long started_waiting_at = millis();
+  while(!Mirf.dataReady())
+  {    
+    if ( ( millis() - started_waiting_at ) > 1000 ) 
+    {
 #if HOME_ATION_DEBUG
-			printf("Timeout on response from server!");
+      printf("Timeout on response from server!");
 #endif
-			return false;
-		}
-	}
-	if (commandArray[0] == 1)//Remote Power Strip
-	{
-		Mirf.getData(response);				
+      return false;
+    }
+  }
+  if (commandArray[0] == 1)//Remote Power Strip
+  {
+    Mirf.getData(response);				
 #if HOME_ATION_DEBUG
-		printf("Got response {%d,%d,%d,%d}\n\r",response[0],response[1],response[2],response[3]);
+    printf("Got response {%d,%d,%d,%d}\n\r",response[0],response[1],response[2],response[3]);
 #endif
-	}
-	return true;		 		
+  }
+  return true;		 		
 }
 
 boolean getCommandFromQuery(char* requestLine, int requestLineLength, byte* commands)
 {        
-    int parameterNumber = 0;    
-    for (int i = 0; i < requestLineLength; i++)
+  int parameterNumber = 0;    
+  for (int i = 0; i < requestLineLength; i++)
+  {
+    char ch = requestLine[i];
+    if (ch == '=')
     {
-      char ch = requestLine[i];
-      if (ch == '=')
-      {
-        commands[parameterNumber] = (byte)atoi(&(requestLine[i+1]));
-        parameterNumber++;
-      }
+      commands[parameterNumber] = (byte)atoi(&(requestLine[i+1]));
+      parameterNumber++;
     }
-    if (parameterNumber == 3)
-    {
+  }
+  if (parameterNumber == 3)
+  {
 #ifdef HOME_ATION_DEBUG
-      printf("id=%d;cmd=%d;param=%d\n\r", commands[0], commands[1], commands[2]);
+    printf("id=%d;cmd=%d;param=%d\n\r", commands[0], commands[1], commands[2]);
 #endif
-      return true;      
-    }
-    else
-    {
+    return true;      
+  }
+  else
+  {
 #ifdef HOME_ATION_DEBUG
-      printf("no params\n\r");
+    printf("no params\n\r");
 #endif
-      return false;
-    }                    
+    return false;
+  }                    
 }
 
 void homePage(uint8_t* response) 
 {
-	bfill.emit_p(PSTR("$F"    
-		"1 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=0\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=0\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=0\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=0\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=0\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=0\">Disable all</a> State - $S" 
-		"2 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=1\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=1\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=1\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=1\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=1\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=1\">Disable all</a> State - $S"  
-		"3 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=2\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=2\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=2\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=2\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=2\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=2\">Disable all</a> State - $S"  
-		"4 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=3\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=3\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=3\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=3\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=3\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=3\">Disable all</a> State - $S" ),
-	http_OK,
-	ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[0] == 0 ? "OFF" : "ON",
-	ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[1] == 0 ? "OFF" : "ON",
-	ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[2] == 0 ? "OFF" : "ON",
-	ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[3] == 0 ? "OFF" : "ON"); 
+  bfill.emit_p(PSTR("$F"    
+    "1 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=0\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=0\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=0\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=0\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=0\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=0\">Disable all</a> State - $S" 
+    "2 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=1\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=1\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=1\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=1\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=1\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=1\">Disable all</a> State - $S"  
+    "3 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=2\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=2\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=2\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=2\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=2\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=2\">Disable all</a> State - $S"  
+    "4 - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=0&param=3\">Enable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=1&param=3\">Disable</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=2&param=3\">Switch</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=3&param=3\">Read</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=4&param=3\">Enable all</a> - <a href=\"http://$D.$D.$D.$D/?id=1&cmd=5&param=3\">Disable all</a> State - $S" ),
+  http_OK,
+  ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[0] == 0 ? "OFF" : "ON",
+  ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[1] == 0 ? "OFF" : "ON",
+  ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[2] == 0 ? "OFF" : "ON",
+  ether.myip[0], ether.myip[1], ether.myip[2], ether.myip[3], response[3] == 0 ? "OFF" : "ON"); 
+}
+
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
 
 void loop() 
 {
   uint8_t response[4];
   boolean hasCommandSend = false;
-	word len = ether.packetReceive();
-	word pos = ether.packetLoop(len); 
-	if (pos) 
-	{
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len); 
+  if (pos) 
+  {
 #ifdef HOME_ATION_DEBUG
     printf("new client\n\r");
 #endif
-		bfill = ether.tcpOffset();
-		char *data = (char *) Ethernet::buffer + pos;				
-          byte commands[3];
-		boolean hasParameters = getCommandFromQuery(data, len, commands);
-          int numberOfRetries = 3;
-          while(hasParameters && !hasCommandSend && numberOfRetries > 0)
-          {
-            numberOfRetries--;
-            hasCommandSend = sendRFCommand(commands, response);    
-          }
-		homePage(response);	
-		ether.httpServerReply(bfill.position());
-      }
+    bfill = ether.tcpOffset();
+    char *data = (char *) Ethernet::buffer + pos;				
+    byte commands[3];
+    boolean hasParameters = getCommandFromQuery(data, len, commands);
+    int numberOfRetries = 3;
+    while(hasParameters && !hasCommandSend && numberOfRetries > 0)
+    {
+      numberOfRetries--;
+      hasCommandSend = sendRFCommand(commands, response);    
     }
+    homePage(response);	
+    ether.httpServerReply(bfill.position());
+  }
+}
+
 
---
+++ HomeAtion/homeation_remotepower_mirf/homeation_remotepower_mirf.ino
@@ -0,0 +1,112 @@
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+#include "printf.h"
+
+static uint8_t myAddress[] =  {0xF0, 0xF0, 0xF0, 0xF0, 0xD2};
+static uint8_t mainAddress[] = {0xF0, 0xF0, 0xF0, 0xF0, 0xE1 };
+
+int socketPins[] = { 2, 3, 4, 5};
+uint8_t socketPinsState[] = { HIGH, HIGH, HIGH, HIGH};
+
+#define HA_REMOTE_POWER_DEBUG 1
+
+void setup(void)
+{
+#if HA_REMOTE_POWER_DEBUG
+  Serial.begin(57600);
+  printf_begin();
+  printf("HomeAtion Remote Power Strip\n\r");
+#endif
+  setupRF();
+  setupRelay();  
+}
+
+void setupRF(void)
+{
+  Mirf.cePin = 7;
+  Mirf.csnPin = 8;
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR(myAddress);
+  Mirf.payload = 4;
+  Mirf.channel = 76;
+  Mirf.config();
+}
+
+void setupRelay()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(socketPins[i], OUTPUT);
+    digitalWrite(socketPins[i], socketPinsState[i]);
+  }
+}
+
+void loop(void)
+{  
+  // if there is data ready
+  if(!Mirf.isSending() && Mirf.dataReady())
+  {
+    // Dump the payloads until we've gotten everything
+    byte command[4];      
+    bool done = false;
+    Mirf.getData(command);     
+#if HA_REMOTE_POWER_DEBUG
+    printf("Read command from radio {%d,%d,%d}\n\r", command[0],command[1],command[2]);
+#endif    
+    if (command[0] == 1)//RemotePower
+    {
+      if (command[1] == 0) //enable 
+      {          
+        digitalWrite(socketPins[command[2]], LOW);
+        socketPinsState[command[2]] = LOW;
+      }
+      else if (command[1] == 1) //disable
+      {          
+        digitalWrite(socketPins[command[2]], HIGH);
+        socketPinsState[command[2]] = HIGH;
+      }
+      else if (command[1] == 2) //switch
+      {
+        if (socketPinsState[command[2]] == LOW)
+        {
+          digitalWrite(socketPins[command[2]], HIGH);
+          socketPinsState[command[2]] = HIGH;
+        }
+        else
+        {
+          digitalWrite(socketPins[command[2]], LOW); 
+          socketPinsState[command[2]] = HIGH;         
+        }
+      } 
+      else if (command[1] == 3)//read
+      {          
+        //do nothing          
+      }
+      else if (command[1] == 4) //enable all
+      {          
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(socketPins[i], LOW);
+          socketPinsState[i] = LOW;          
+        }
+      }
+      else if (command[1] == 5) //disable all
+      {          
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(socketPins[i], HIGH);
+          socketPinsState[i] = HIGH;          
+        }          
+      }
+    }        
+    Mirf.setTADDR(mainAddress);
+    Mirf.send(socketPinsState);
+#if HA_REMOTE_POWER_DEBUG
+    printf("Sent state response {%d,%d,%d,%d}\n\r", socketPinsState[0],socketPinsState[1],socketPinsState[2],socketPinsState[3]);
+#endif        
+  } 
+}
+
